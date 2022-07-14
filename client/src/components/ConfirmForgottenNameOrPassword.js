a5c7546288fcdb69bee34e3768eb5f213d149050
hachi-ops <67706415+hachi-ops@users.noreply.github.com>
2022-07-14 19:31:38 +0100

    Update ConfirmForgottenNameOrPassword.js

--- client/src/components/ConfirmForgottenNameOrPassword.js
+++ client/src/components/ConfirmForgottenNameOrPassword.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function ConfirmForgottenNameOrPassword() {
-  return <h2>Login to you email to reset your login details</h2>;
+  const navigate = useNavigate();
+  return (
+    <>
+      <button onClick={() => navigate(-1)}>back</button>
+      <h2>Login to you email to reset your login details</h2>
+    </>
+  );
 }
 
 export default ConfirmForgottenNameOrPassword;
