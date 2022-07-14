5e1f5945bb825b17473cbfbcfcb5f8b7e2a45e92
hachi-ops <67706415+hachi-ops@users.noreply.github.com>
2022-07-15 00:14:30 +0100

    Update GuestLogin.js
    Guest login back button

--- client/src/components/GuestLogin.js
+++ client/src/components/GuestLogin.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { Form } from "./styles/Container.styled";
+import { useNavigate } from "react-router-dom";
 
 function GuestLogin() {
+  const navigate = useNavigate();
   return (
     <>
+      <button onClick={() => navigate(-1)}>back</button>
       <Form>
         <label>First Name</label>
         <input type="text" />
