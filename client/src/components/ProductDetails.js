ddf6470dc6ba5646813aa90fc0df8b654ad7157d
hachi-ops <67706415+hachi-ops@users.noreply.github.com>
2022-07-15 01:53:41 +0100

    Update ProductDetails.js
    navigation route changed

--- client/src/components/ProductDetails.js
+++ client/src/components/ProductDetails.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 function ProductDetails() {
   const navigate = useNavigate();
+  const params = useParams();
+  const productId = params.productId;
   return (
     <>
       <div>
         <button onClick={() => navigate(-1)}>back</button>
         <p>Product name</p>
+        <p>Product id {productId}</p>
         <p>Product description</p>
         <img alt="" />
 
-        <button onClick={() => navigate("/request-form")}>place request</button>
+        <button onClick={() => navigate("/user-login")}>place request</button>
       </div>
     </>
   );
