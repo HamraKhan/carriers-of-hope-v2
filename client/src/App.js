e1cc446d2ce1e1bad3beaa5c05bdddf5faae095c
hachi-ops <67706415+hachi-ops@users.noreply.github.com>
2022-07-18 22:29:16 +0100

    rendering dashboard-customers-list route

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,23 @@
-import "./App.css"
-import { Routes, Route } from "react-router-dom"
-import Homepage from "./Pages/Homepage"
-import ProductsPage from "./Pages/ProductsPage"
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./Pages/Homepage";
+import ProductsPage from "./Pages/ProductsPage";
 import ProductDetails from "./Pages/ProductDetails";
-import RequestForm from "./components/RequestForm"
+import RequestForm from "./components/RequestForm";
 import LoginPage from "./Pages/LoginPage";
-import Navbar from "./components/Navbar"
-import OrderSummary from "./components/OrderSummary"
-import Footer from "./components/Footer"
-import NoMatch from "./components/NoMatch"
-import Global from './styles/global'
-import ProductsProvider from "./services/products.context"
-import FilteredProducts from "./components/FilteredProducts"
-import ForgottenNameOrPassword from "./components/ForgottenNameOrPassword"
-import SignUp from "./components/SignUp"
-import { Container } from './styles/Container.styled'
+import Navbar from "./components/Navbar";
+import OrderSummary from "./components/OrderSummary";
+import Footer from "./components/Footer";
+import NoMatch from "./components/NoMatch";
+// import Global from "./styles/global";
+import ProductsProvider from "./services/products.context";
+import FilteredProducts from "./components/FilteredProducts";
+import ForgottenNameOrPassword from "./components/ForgottenNameOrPassword";
+import SignUp from "./components/SignUp";
+import Dashboard from "./Pages/Dashboard";
+import DashboardCustomersList from "./Pages/DashboardCustomersList";
 
-
-
-
+import { Container } from "./styles/Container.styled";
 
 function App() {
   return (
@@ -26,36 +25,39 @@ function App() {
       <Container>
         <Navbar />
         <div className="content-wrap">
-        <ProductsProvider>
-          <Routes>
-            <Route path="/" exact element={<Homepage />}></Route>
-            <Route path="/products"  element={<ProductsPage />}></Route>
-            <Route
-            
-              path="/product-details/:id"
-              element={<ProductDetails />}
-            ></Route>
-            <Route path="filtered-products" element={<FilteredProducts />} />
+          <ProductsProvider>
+            <Routes>
+              <Route path="/" exact element={<Homepage />}></Route>
+              <Route path="/products" element={<ProductsPage />}></Route>
+              <Route
+                path="/product-details/:id"
+                element={<ProductDetails />}
+              ></Route>
+              <Route path="filtered-products" element={<FilteredProducts />} />
 
-            <Route path="login" element={<LoginPage />} />
-            <Route
-              path="forgotten-name-or-password"
-              element={<ForgottenNameOrPassword />}
-            ></Route>
-            <Route path="sign-up" element={<SignUp />}></Route>
-            <Route></Route>
-            <Route path="request-form" element={<RequestForm />}></Route>
-            <Route path="order-summary" element={<OrderSummary />}></Route>
-            <Route path="*"  element={<NoMatch />}></Route>
-          </Routes>
-        </ProductsProvider>
+              <Route path="login" element={<LoginPage />} />
+              <Route
+                path="forgotten-name-or-password"
+                element={<ForgottenNameOrPassword />}
+              ></Route>
+              <Route path="sign-up" element={<SignUp />}></Route>
+              <Route></Route>
+              <Route path="request-form" element={<RequestForm />}></Route>
+              <Route path="order-summary" element={<OrderSummary />}></Route>
+              <Route path="dashboard" element={<Dashboard />}></Route>
+              <Route
+                path="dashboard-customers-list"
+                element={<DashboardCustomersList />}
+              ></Route>
+              <Route path="*" element={<NoMatch />}></Route>
+            </Routes>
+          </ProductsProvider>
         </div>
         <Footer />
       </Container>
     </>
-  )
+  );
 }
-
 
 // function App() {
 //   return (
@@ -82,9 +84,7 @@ function App() {
 //   )
 // }
 
-
-export default App
-
+export default App;
 
 //! This is the old App() from the merge. This is for reference
 // function App() {
