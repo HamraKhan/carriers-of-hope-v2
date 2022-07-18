2c8d6951432c4c47ecb042cdbae1d26fe6543a8c
hachi-ops <67706415+hachi-ops@users.noreply.github.com>
2022-07-18 22:30:52 +0100

    Create Dashboard.js
    renedered AdminButtons component inside Dashboard.js

---
+++ client/src/Pages/Dashboard.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import { Routes, Route } from "react-router-dom";
+// import { useState } from "react";
+// import { useEffect } from "react";
+import AdminButtons from "../components/AdminButtons";
+
+function Dashboard() {
+  return (
+    <>
+      <AdminButtons />
+    </>
+  );
+}
+
+export default Dashboard;
